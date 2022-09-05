0a153c75038c1f803dc2f9517568d169f47ff95d
teranteks <jenkyfonk@gmail.com>
2022-09-05 16:14:26 +0300

    add test for python app, edit workflow sequence of work

---
+++ app_code/tests/test_app.py
@@ -0,0 +1,26 @@
+import  requests, json
+import pytest
+from flask import Flask
+
+from app_code.main import create_app
+
+@pytest.fixture()
+def client():
+    app = create_app()
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture()
+def set_url():
+    url="http://localhost:5000/healthcheck"
+    return url
+
+
+def test_webserver_reply(set_url,client):
+
+    response = client.get(set_url)    
+    assert response.status_code == 200
+   
+
