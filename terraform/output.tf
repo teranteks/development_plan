cf5fb61ddeff51a6a022498fac75bfaec0de1c58
teranteks <jenkyfonk@gmail.com>
2022-09-07 11:05:33 +0300

    Added sensetive attribute to output and vars files

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,9 @@
-output "pool_size" {
-  value = linode_lke_cluster.k8s_ter.pool
-  description = "lke pool size"
+output "cluster_label" {
+  value = linode_lke_cluster.k8s_ter.label
+  description = "lke label"
   depends_on = [
     linode_lke_cluster.k8s_ter
   ]
+  sensitive = true
 }
 
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,6 +1,7 @@
 variable "linode_token" {
     type = string
     description = "token for auth in linode"
+    sensitive = true
   
 }
 
@@ -12,20 +13,23 @@ variable "k8s_version" {
 variable "k8s_cluster_name" {
     type = string
     description = "cluster name"
-  
+    sensitive = true
 }
 
 variable "cluster_region" {
   type = string
   description = "k8s cluster region"
+  sensitive = true  
 }
 
 variable "cluster_pool_count" {
   type = number
   description = "number of nodes for cluster"
+  sensitive = true
 }
 
 variable "cluster_pool_type" {
   type = string
   description = "type of pool nodes"
+
 }
